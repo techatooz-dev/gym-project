1ef75bd90e2efa801e7efad1a25a5859bb98bd54
saleemmughal17 <saleem177mughal@gmai.com>
2025-09-26 11:10:50 +0500

    benifits

--- gym/src/components/Benifits/IntroSection.jsx
+++ gym/src/components/Benifits/IntroSection.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 function IntroSection() {
   return (
     <section className="relative bg-white">
-      <div className="mx-auto max-w-7xl  py-8 md:py-12">
+      <div className="mx-auto max-w-7xl ">
         <div className="relative overflow-hidden rounded-md">
           <Image
             src="/Group 1.png"
