26d299dd1f3eef318379f1874db6f4b63697c300
saleemmughal17 <saleem177mughal@gmai.com>
2025-09-29 11:49:18 +0500

    *

--- gym/src/config/site.js
+++ gym/src/config/site.js
@@ -2,14 +2,14 @@ export const site = {
   name: "ProEMSFitness",
   tagline: "save money and time with electronic muscle stimulation",
   logo: {
-    src: "/Logo-02.png", // located in /public
+    src: "/Logo-023.png", // located in /public
     alt: "ProEMSFitness logo",
     width: 220,
     height: 110,
   },
   // Separate footer logo (can be swapped independently of main logo)
   footerLogo: {
-    src: "/footer_logo-01.png",
+    src: "/Logo-023 white.png",
     alt: "ProEMSFitness footer logo",
     width:160,
     height: 120,
